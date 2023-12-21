733e933ae424b2288949173def6a0c474e6c1c44
Damian Osinski <osinskidamian03@gmail.com>
2023-12-21 10:35:15 +0100

    Add mobile nav

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 import "/style.scss"
+
+const nav = document.querySelector(".nav");
+const menuOptions = nav.querySelectorAll("a");
+const navButton = document.querySelector(".nav-button");
+
+navButton.onclick = () => {
+    nav.classList.toggle("active");
+    navButton.classList.toggle("active");
+}
+
+menuOptions.forEach((option) => {
+    option.onclick = () => {
+        nav.classList.remove("active");
+        navButton.classList.remove("active");
+    }
+})
